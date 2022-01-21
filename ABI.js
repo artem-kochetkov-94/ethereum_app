1cb94107b3921abfbcc60e80f1aedd9fe4685253
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2022-01-21 21:01:10 +0300

    create contract

---
+++ ABI.js
@@ -0,0 +1,24 @@
+module.exports = {
+    helloWorld: [
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "Hello",
+            "outputs": [
+                {
+                    "name": "",
+                    "type": "string"
+                }
+            ],
+            "payable": false,
+            "type": "function",
+            "stateMutability": "view"
+        },
+        {
+            "inputs": [],
+            "type": "constructor",
+            "payable": true,
+            "stateMutability": "payable"
+        }
+    ],
+};
